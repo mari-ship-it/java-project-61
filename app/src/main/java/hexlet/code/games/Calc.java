92cefd16ba50a58cc403b5ea7d79ed4c7ef3f029
Marina Sirenko <msirenko96@gmail.com>
2025-02-07 12:36:25 +0300

    update is a magic number

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,6 +5,7 @@ import hexlet.code.Engine;
 public class Calc {
 
     protected static final int MAX_VALUE_RANDOM_NUM = 100;
+    private static final int MAX_VALUE_FOR_THE_COMPARISON_OPERATOR = 4;
 
     public static void launchTheGameCalc() {
         String exercise = "What is the result of the expression?";
@@ -14,7 +15,7 @@ public class Calc {
         for (int i = 0; i < Engine.getMaxNumbersOfRounds(); i++) {
             int firstNum = Engine.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
             int secondNum = Engine.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
-            int operatorNum = Engine.generateRandomNumber(1, 4);
+            int operatorNum = Engine.generateRandomNumber(1, MAX_VALUE_FOR_THE_COMPARISON_OPERATOR);
             switch (operatorNum) {
                 case 1:
                     questions[i] = firstNum + " + " + secondNum;
