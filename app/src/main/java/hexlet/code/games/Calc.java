70933230bd8d579c4edd16b9b7d0cde19c4ae786
Marina Sirenko <msirenko96@gmail.com>
2025-02-11 14:20:40 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -9,29 +9,47 @@ public class Calc {
     private static final int MAX_VALUE_FOR_THE_COMPARISON_OPERATOR = 4;
 
     public static void launchTheGameCalc() {
-        String exercise = "What is the result of the expression?";
-        String[] questions = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
-        String[] correctAnswers = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
 
-        for (int i = 0; i < Engine.MAX_NUMBERS_OF_ROUNDS; i++) {
+        String exercise = "What is the result of the expression?";
+        String[][] roundsData = new String[Engine.MAX_NUMBERS_OF_ROUNDS][2];
+
+        int i = 0;
+        while (i < Engine.MAX_NUMBERS_OF_ROUNDS) {
             int firstNum = Utils.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
             int secondNum = Utils.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
             int operatorNum = Utils.generateRandomNumber(1, MAX_VALUE_FOR_THE_COMPARISON_OPERATOR);
+            String operator;
+
             switch (operatorNum) {
                 case 1:
-                    questions[i] = firstNum + " + " + secondNum;
-                    correctAnswers[i] = Integer.toString(firstNum + secondNum);
+                    operator = "+";
                     break;
                 case 2:
-                    questions[i] = firstNum + " - " + secondNum;
-                    correctAnswers[i] = Integer.toString(firstNum - secondNum);
+                    operator = "-";
+                    break;
+                case 3:
+                    operator = "*";
                     break;
                 default:
-                    questions[i] = firstNum + " * " + secondNum;
-                    correctAnswers[i] = Integer.toString(firstNum * secondNum);
-                    break;
+                    throw new IllegalStateException("Unexpected value: " + operatorNum);
             }
+            roundsData[i][Engine.QUESTION_INDEX] = firstNum + " " + operator + " " + secondNum;
+            roundsData[i][Engine.ANSWER_INDEX] = Integer.toString(calculate(operator, firstNum, secondNum));
+            i++;
         }
-        Engine.setLogicOfGame(exercise, questions, correctAnswers);
+        Engine.setLogicOfGame(exercise, roundsData);
+    }
+
+    private static int calculate(String operator, int firstNum, int secondNum) {
+        switch (operator) {
+            case "+":
+                return firstNum + secondNum;
+            case "-":
+                return firstNum - secondNum;
+            case "*":
+                return firstNum * secondNum;
+            default:
+                throw new IllegalStateException("Unexpected value: " + operator);
+        }
     }
 }
