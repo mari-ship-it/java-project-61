887dea7c30f8e2f106e8a0a692d6fa80adca8653
Marina Sirenko <msirenko96@gmail.com>
2025-02-11 10:38:01 +0300

    update Even.java

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -7,17 +7,17 @@ public class Even {
 
     public static void launchTheGameEven() {
         String exercise = "Answer 'yes' if the number is even, otherwise answer 'no'.";
-        String[] questions = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
-        String[] correctAnswers = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
+        String[][] roundsData = new String[Engine.MAX_NUMBERS_OF_ROUNDS][2];
 
         for (var i = 0; i < Engine.MAX_NUMBERS_OF_ROUNDS; i++) {
             int randomNum = Utils.generateRandomNumber(1, Calc.MAX_VALUE_RANDOM_NUM);
-            String strRandomNum = Integer.toString(randomNum);
-            String corrAnswer = randomNum % 2 == 0 ? "yes" : "no";
-            questions[i] = strRandomNum;
-            correctAnswers[i] = corrAnswer;
+            roundsData[i][Engine.QUESTION_INDEX] = Integer.toString(randomNum);
+            roundsData[i][Engine.ANSWER_INDEX] = isEven(randomNum) ? "yes" : "no";
         }
+        Engine.setLogicOfGame(exercise, roundsData);
+    }
 
-        Engine.setLogicOfGame(exercise, questions, correctAnswers);
+    private static boolean isEven(int randomNum) {
+        return randomNum % 2 == 0;
     }
 }
