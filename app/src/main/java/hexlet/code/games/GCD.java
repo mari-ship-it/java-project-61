ccf536c6c3e879c2314d12f562bb0a33ce39a44f
Marina Sirenko <msirenko96@gmail.com>
2025-02-11 14:29:37 +0300

    update GCD.java

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -8,19 +8,18 @@ public class GCD {
     public static void launchTheGameGCD() {
 
         String exercise = "Find the greatest common divisor of given numbers.";
-        String[] questions = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
-        String[]  correctAnswers = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
+        String[][] roundsData = new String[Engine.MAX_NUMBERS_OF_ROUNDS][2];
 
         for (int i = 0; i < Engine.MAX_NUMBERS_OF_ROUNDS; i++) {
             int firstNum = Utils.generateRandomNumber(1, Calc.MAX_VALUE_RANDOM_NUM);
             int randomNum2 = Utils.generateRandomNumber(1, Calc.MAX_VALUE_RANDOM_NUM);
-            questions[i] = firstNum + " " + randomNum2;
-            correctAnswers[i] = Integer.toString(getGreatestCommonDivisor(firstNum, randomNum2));
+            roundsData[i][Engine.QUESTION_INDEX] = firstNum + " " + randomNum2;
+            roundsData[i][Engine.ANSWER_INDEX] = Integer.toString(getGreatestCommonDivisor(firstNum, randomNum2));
         }
-        Engine.setLogicOfGame(exercise, questions, correctAnswers);
+        Engine.setLogicOfGame(exercise, roundsData);
     }
 
-    public static int getGreatestCommonDivisor(int num1, int num2) {
+    private static int getGreatestCommonDivisor(int num1, int num2) {
         while (num1 != num2) {
             if (num1 > num2) {
                 num1 -= num2;
