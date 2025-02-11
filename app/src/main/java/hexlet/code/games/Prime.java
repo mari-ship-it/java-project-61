8a32f9eaae8821618bcbb0e6f018151908708e96
Marina Sirenko <msirenko96@gmail.com>
2025-02-11 14:48:31 +0300

    update Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -4,22 +4,21 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public class Prime {
+
     public static void launchTheGamePrime() {
+
         String exercise = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
-        String[] questions = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
-        String[] correctAnswers = new String[Engine.MAX_NUMBERS_OF_ROUNDS];
+        String[][] roundsData = new String[Engine.MAX_NUMBERS_OF_ROUNDS][2];
 
         for (int i = 0; i < Engine.MAX_NUMBERS_OF_ROUNDS; i++) {
             int randomNum = Utils.generateRandomNumber(1, Calc.MAX_VALUE_RANDOM_NUM);
-            questions[i] = Integer.toString(randomNum);
-
-            correctAnswers[i] = randomNum < 2 || hasDivisorsOfNumber(randomNum) ? "no" : "yes";
-
+            roundsData[i][Engine.QUESTION_INDEX] = Integer.toString(randomNum);
+            roundsData[i][Engine.ANSWER_INDEX] = randomNum < 2 || hasDivisorsOfNumber(randomNum) ? "no" : "yes";
         }
-        Engine.setLogicOfGame(exercise, questions, correctAnswers);
+        Engine.setLogicOfGame(exercise, roundsData);
     }
 
-    public static boolean hasDivisorsOfNumber(int randomNum) {
+    private static boolean hasDivisorsOfNumber(int randomNum) {
         int i = 2;
         while (i < randomNum) {
             if (randomNum % i == 0) {
