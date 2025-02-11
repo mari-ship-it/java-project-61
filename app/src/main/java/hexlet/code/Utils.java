d54f561a64000f37a95e77ebf880de3f4adf6f53
Marina Sirenko <msirenko96@gmail.com>
2025-02-11 18:04:22 +0300

    add new method to Utils.java

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,11 +1,23 @@
 package hexlet.code;
 
 import java.util.Random;
+import static hexlet.code.games.Progression.PROGRESSION_LENGTH;
 
 public class Utils {
 
     public static int generateRandomNumber(int minNum, int maxNum) {
         Random random = new Random();
         return random.nextInt(minNum, maxNum);
+    }
+
+    public static String[] generateOfArithmeticProgression(int randomNum, int stepOfProgression) {
+        String[] arithmeticProgression = new String[PROGRESSION_LENGTH];
+        int currentElement = randomNum;
+
+        for (int i = 0; i < PROGRESSION_LENGTH; i++) {
+            arithmeticProgression[i] = Integer.toString(currentElement);
+            currentElement += stepOfProgression;
+        }
+        return arithmeticProgression;
     }
 }
