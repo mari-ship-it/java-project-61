8a105d6063e0e50a06b1d96915f7bf9b0da5b387
Marina Sirenko <msirenko96@gmail.com>
2025-02-15 15:07:29 +0300

    update Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,43 +5,33 @@ import hexlet.code.Utils;
 
 public class Calc {
 
-    protected static final int MAX_VALUE_RANDOM_NUM = 100;
-    private static final int MAX_VALUE_FOR_THE_COMPARISON_OPERATOR = 4;
-
     public static void launchTheGameCalc() {
 
-        String exercise = "What is the result of the expression?";
+        final int maxValueRandomNum = 100;
+        final int minValueRandomNum = 0;
+        final String[] operators = new String[]{"+", "-", "*"};
+        final String exercise = "What is the result of the expression?";
         String[][] roundsData = new String[Engine.MAX_NUMBERS_OF_ROUNDS][2];
-
         int i = 0;
-        while (i < Engine.MAX_NUMBERS_OF_ROUNDS) {
-            int firstNum = Utils.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
-            int secondNum = Utils.generateRandomNumber(1, MAX_VALUE_RANDOM_NUM);
-            String operatorNum = Integer.toString(Utils.generateRandomNumber(1, MAX_VALUE_FOR_THE_COMPARISON_OPERATOR));
-            String operator;
 
-            switch (operatorNum) {
-                case "1":
-                    operator = "+";
-                    break;
-                case "2":
-                    operator = "-";
-                    break;
-                case "3":
-                    operator = "*";
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + operatorNum);
-            }
-            roundsData[i][Engine.QUESTION_INDEX] = firstNum + " " + operator + " " + secondNum;
-            roundsData[i][Engine.ANSWER_INDEX] = Integer.toString(calculate(operator, firstNum, secondNum));
+        while (i < Engine.MAX_NUMBERS_OF_ROUNDS) {
+
+            final int firstNum = Utils.generateRandomNumber(minValueRandomNum, maxValueRandomNum);
+            final int secondNum = Utils.generateRandomNumber(minValueRandomNum, maxValueRandomNum);
+            final int operatorNum = Utils.generateRandomNumber(minValueRandomNum, operators.length);
+
+            roundsData[i][Engine.QUESTION_INDEX] = firstNum + " " + operators[operatorNum] + " " + secondNum;
+            roundsData[i][Engine.ANSWER_INDEX] = Integer.toString(calculate(operators[operatorNum],
+                    firstNum, secondNum));
             i++;
         }
         Engine.setLogicOfGame(exercise, roundsData);
     }
 
     private static int calculate(String operator, int firstNum, int secondNum) {
+
         switch (operator) {
+
             case "+":
                 return firstNum + secondNum;
             case "-":
